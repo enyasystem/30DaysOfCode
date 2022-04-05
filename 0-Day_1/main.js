3d89d8935a4a4c3e7d2b422e962c11de36a26444
enyasystem <enyaelvis@gmail.com>
2022-04-05 22:11:07 +0100

    Js file created

---
+++ 0-Day_1/main.js
@@ -0,0 +1,12 @@
+       function sum(arr) {  
+        let sum = 0; 
+        // initialize sum  
+    
+        // Iterate through all elements  
+        // and add them to sum  
+        for (let i = 0; i < arr.length; i++)  
+            sum += arr[i];  
+        return sum;  
+    }  
+    let arr = [0, 3, 2, 5];
+    document.write("Sum of given array is " + sum(arr));
